254c6303083a8d461400f49e8bb51e07eb0e2835
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-31 08:42:27 -0400

    Initial commit

---
+++ src/main/java/me/rallings/springboot/JavaDay21Springboot1Application.java
@@ -0,0 +1,13 @@
+package me.rallings.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDay21Springboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDay21Springboot1Application.class, args);
+    }
+
+}
